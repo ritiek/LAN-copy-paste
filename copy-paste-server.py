9ae0c7b36ffe121aa417cf311671cc7fed738609
Ritiek Malhotra <Ritiek@users.noreply.github.com>
2017-02-19 10:27:13 +0530

    send message on text upload

--- copy-paste-server.py
+++ copy-paste-server.py
@@ -17,7 +17,7 @@ def my_form_post():
 	global defaults
         defaults = format(request.form['text'])
 	print defaults
-        template = '<head><meta name="viewport" content="width=device-width"></head><form action="." method="POST"><textarea id="text" name="text" cols="60" rows="10">' + defaults + '</textarea><br><input type="submit" name="my-form" value="Upload Text"></form>'
+        template = '<head><meta name="viewport" content="width=device-width"></head><form action="." method="POST"><textarea id="text" name="text" cols="60" rows="10">' + defaults + '</textarea><br><input type="submit" name="my-form" value="Upload Text">Text was uploaded successfully!</form>'
 	return template
 
 if __name__ == '__main__':
